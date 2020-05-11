f418eaf17dc2885a21f677ec23eb72cfb9e3dd70
Zack Jackson <zackary.l.jackson@gmail.com>
2020-05-10 20:24:28 -0700

    feat: add stringifid classe (#1)

--- dashboard-plugin/FederationDashboardPlugin.js
+++ dashboard-plugin/FederationDashboardPlugin.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
+const {parse, stringify} = require("flatted/cjs");
 
 /** @typedef {import('webpack/lib/Compilation')} Compilation */
 /** @typedef {import('webpack/lib/Compiler')} Compiler */
@@ -41,6 +43,9 @@ class FederationDashboardPlugin {
        */
       (compilation, callback) => {
         const stats = compilation.getStats().toJson();
+        const liveStats = compilation.getStats();
+        console.log("liveStats", liveStats);
+
         const hashPath = path.join(
           compilation.options.output.path,
           this._options.filename
@@ -59,33 +64,100 @@ class FederationDashboardPlugin {
           ];
           return array.some((item) => item);
         });
-
         const RemoteEntryChunk = stats.chunks.find((chunk) => {
           const specificChunk = chunk.names.find((name) => {
             return name === FederationPluginOptions.name;
           });
           return specificChunk;
         });
-        const dashData = (this._dashData = {
+        console.log("remoteChunk", RemoteEntryChunk);
+        console.log(
+          "Federation getter async",
+          liveStats.compilation.namedChunks.get(FederationPluginOptions.name)
+        );
+        console.log(
+          "Federation getAllAsyncChunks",
+          liveStats.compilation.namedChunks
+            .get(FederationPluginOptions.name)
+            .getAllAsyncChunks()
+        );
+        const AllReferencedChunksByRemote = liveStats.compilation.namedChunks
+          .get(FederationPluginOptions.name)
+          .getAllReferencedChunks();
+
+        console.log(
+          "Federation getAllReferencedChunks",
+          liveStats.compilation.namedChunks
+            .get(FederationPluginOptions.name)
+            .getAllAsyncChunks()
+        );
+        const validChunkArray = [];
+        AllReferencedChunksByRemote.forEach((chunk) => {
+          if (chunk.id !== FederationPluginOptions.name) {
+            validChunkArray.push(chunk);
+          }
+        });
+
+        function isIterable(obj) {
+          // checks for null and undefined
+          if (obj == null) {
+            return false;
+          }
+          return typeof obj[Symbol.iterator] === 'function';
+        }
+
+        function mapToObjectRec(m) {
+          let lo = {}
+            for (let [k, v] of Object.entries(m)) {
+              if (v instanceof Map) {
+                lo[k] = mapToObjectRec(v)
+              } else if (v instanceof Set) {
+                lo[k] = mapToObjectRec(Array.from(v))
+              } else {
+                lo[k] = v
+              }
+            }
+          return lo
+        }
+
+        console.log("valid chunks", validChunkArray);
+        const chunkDependencies = validChunkArray.reduce((acc, chunk) => {
+          const subset = chunk.getAllReferencedChunks()
+         const stringifiableChunk =  Array.from(subset).map((sub) => {
+          const  cleanSet =  Object.getOwnPropertyNames(sub).reduce((acc, key) => {
+            if(key === '_groups') return acc
+             return Object.assign(acc, {[key]: sub[key]})
+            }, {})
+            return mapToObjectRec(cleanSet)
+          })
+          return Object.assign(acc, {
+            [chunk.id]: stringifiableChunk,
+          });
+        }, {});
+
+        console.log("chunkDependencies", chunkDependencies);
+        const dashData = (this._dashData = JSON.stringify({
           publicPath: compilation.outputOptions.publicPath,
           federationRemoteEntry: RemoteEntryChunk,
           buildHash: stats.hash,
           modules,
-        });
+          chunkDependencies
+        }))
+
         Promise.all([
           new Promise((resolve) => {
             fs.writeFile(
               hashPath,
-              JSON.stringify(dashData, null, 2),
-              { encoding: "utf-8" },
+              dashData,
+              {encoding: "utf-8"},
               resolve
             );
           }),
           new Promise((resolve) => {
             fs.writeFile(
               statsPath,
-              JSON.stringify(stats, null, 2),
-              { encoding: "utf-8" },
+              JSON.stringify(stats),
+              {encoding: "utf-8"},
               resolve
             );
           }),
@@ -94,6 +166,7 @@ class FederationDashboardPlugin {
     );
 
     compiler.hooks.afterDone.tap(PLUGIN_NAME, (stats) => {
+      console.log("statsJSON", stats.toJson());
       if (this._options.reportFunction) {
         this._options.reportFunction(this._dashData);
       }
