ab6edf003f96cf758f4b1c82bfebc660a84cf384
Jack Herrington <jherr@pobox.com>
2020-05-12 16:53:05 -0700

    Externalized the graph creation

--- report.sh
+++ report.sh
@@ -3,3 +3,4 @@ cp dashboard-example/dsl/dist/dashboard.json dashboard-fe/data/dsl.json
 cp dashboard-example/home/dist/dashboard.json dashboard-fe/data/home.json
 cp dashboard-example/search/dist/dashboard.json dashboard-fe/data/search.json
 cp dashboard-example/nav/dist/dashboard.json dashboard-fe/data/nav.json
+node dashboard-fe/scripts/convert-to-graph.js dsl.json home.json search.json nav.json
