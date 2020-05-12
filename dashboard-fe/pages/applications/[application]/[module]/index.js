8087a0622cb4d51794f18917d57f79762b6cad4d
Jack Herrington <jherr@pobox.com>
2020-05-12 16:43:05 -0700

    More routes

--- dashboard-fe/pages/applications/[application]/[module]/index.js
+++ dashboard-fe/pages/applications/[application]/[module]/index.js
@@ -1,33 +1,27 @@
 import Head from "next/head";
-import { makeStyles } from "@material-ui/core";
-import Layout from "../../../../components/Layout";
+import { makeStyles, Typography, Grid, Paper } from "@material-ui/core";
+import Link from "next/link";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { useRouter } from "next/router";
+import Layout from "../../../../components/Layout";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: 10,
+  },
+  panel: {
+    padding: 10,
+  },
+}));
 
-const GET_APPS = gql`
-  {
-    applications {
+const GET_MODULES = gql`
+  query($application: String!, $name: String!) {
+    modules(application: $application, name: $name) {
       id
+      file
       name
-      modules {
-        id
-        name
-        requires {
-          name
-        }
-      }
-      overrides {
-        id
-        name
-      }
-      consumes {
-        application {
-          id
-          name
-        }
+      requires {
         name
       }
     }
@@ -37,13 +31,51 @@ const GET_APPS = gql`
 const ModulePage = () => {
   const classes = useStyles();
   const router = useRouter();
+  const { data } = useQuery(GET_MODULES, {
+    variables: {
+      name: router.query.module,
+      application: router.query.application,
+    },
+  });
+  console.log(data);
+
   return (
     <Layout>
       <Head>
         <title>Federated Modules Dashboard</title>
       </Head>
-      <div>
-        Module: {router.query.application} - {router.query.module}
+      <div className={classes.container}>
+        {data &&
+          data.modules.map((mod) => (
+            <div>
+              <Typography variant="h4">
+                <Link href={`/applications/${router.query.application}`}>
+                  {router.query.application}
+                </Link>
+                /{mod.name}
+              </Typography>
+              <Grid container spacing={3}>
+                <Grid item xs={6}>
+                  <Paper elevation={3} className={classes.panel}>
+                    <Typography variant="h6" className={classes.panelTitle}>
+                      File
+                    </Typography>
+                    <Typography>{mod.file}</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                  <Paper elevation={3} className={classes.panel}>
+                    <Typography variant="h6" className={classes.panelTitle}>
+                      Requires
+                    </Typography>
+                    <Typography>
+                      {mod.requires.map(({ name }) => name).join()}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </div>
+          ))}
       </div>
     </Layout>
   );
