d5a7b2211de9a6d7f155eccb6df629570c4aa6ef
Jack Herrington <jherr@pobox.com>
2020-05-15 21:27:06 -0700

    Adding a chord chart (#4)

--- dashboard-fe/pages/index.js
+++ dashboard-fe/pages/index.js
@@ -6,6 +6,8 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Tabs,
+  Tab,
   makeStyles,
 } from "@material-ui/core";
 import gql from "graphql-tag";
@@ -13,6 +15,7 @@ import { useQuery } from "@apollo/react-hooks";
 import UpArrow from "@material-ui/icons/PublishTwoTone";
 import DownArrow from "@material-ui/icons/CloudDownloadOutlined";
 import Link from "next/link";
+import { ResponsiveChord } from "@nivo/chord";
 
 import Layout from "../components/Layout";
 
@@ -168,15 +171,121 @@ const Applications = ({ applications }) => {
   );
 };
 
+const ModuleChordChart = ({ applications }) => {
+  const modules = applications.map(({ modules }) => modules).flat();
+  const columns = applications.length + modules.length;
+  const matrix = new Array(columns)
+    .fill(0)
+    .map(() => new Array(columns).fill(0));
+  const keys = [];
+  const appById = {};
+  const modulesById = {};
+  applications.forEach(({ id, name: appName, modules }) => {
+    appById[id] = keys.length;
+    keys.push(appName);
+    modules.forEach(({ name: moduleName }) => {
+      modulesById[`${appName}/${moduleName}`] = keys.length;
+      keys.push(`${appName}/${moduleName}`);
+    });
+  });
+  applications.forEach(({ id, consumes }) => {
+    consumes.forEach(({ application: { name: appName }, name, usedIn }) => {
+      const modId = `${appName}/${name}`;
+      matrix[appById[id]][modulesById[modId]] = usedIn.length;
+      matrix[modulesById[modId]][appById[id]] = usedIn.length;
+    });
+  });
+
+  return (
+    <div
+      style={{
+        height: 1200,
+        width: "100%",
+        marginTop: 50,
+      }}
+    >
+      <ResponsiveChord
+        matrix={matrix}
+        keys={keys}
+        margin={{ top: 60, right: 60, bottom: 90, left: 60 }}
+        valueFormat=".2f"
+        padAngle={0.02}
+        innerRadiusRatio={0.96}
+        innerRadiusOffset={0.02}
+        arcOpacity={1}
+        arcBorderWidth={1}
+        arcBorderColor={{ from: "color", modifiers: [["darker", 0.4]] }}
+        ribbonOpacity={0.5}
+        ribbonBorderWidth={1}
+        ribbonBorderColor={{ from: "color", modifiers: [["darker", 0.4]] }}
+        enableLabel={true}
+        label="id"
+        labelOffset={12}
+        labelRotation={-90}
+        labelTextColor={{ from: "color", modifiers: [["darker", 1]] }}
+        colors={{ scheme: "nivo" }}
+        isInteractive={true}
+        arcHoverOpacity={1}
+        arcHoverOthersOpacity={0.25}
+        ribbonHoverOpacity={0.75}
+        ribbonHoverOthersOpacity={0.25}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={7}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            justify: false,
+            translateX: 0,
+            translateY: 70,
+            itemWidth: 80,
+            itemHeight: 14,
+            itemsSpacing: 0,
+            itemTextColor: "#999",
+            itemDirection: "left-to-right",
+            symbolSize: 12,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
 const Home = () => {
   const { data } = useQuery(GET_APPS);
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Layout>
       <Head>
         <title>Federated Modules Dashboard</title>
       </Head>
-      {data && <Applications applications={data.applications} />}
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <Tab label="Dependency Graph" />
+        <Tab label="Dependency Table" />
+      </Tabs>
+      {data && value === 0 && (
+        <ModuleChordChart applications={data.applications} />
+      )}
+      {data && value === 1 && <Applications applications={data.applications} />}
     </Layout>
   );
 };
