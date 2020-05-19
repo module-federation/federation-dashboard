cfd740250f220eec71b9262d323146619df952e6
Jack Herrington <jherr@pobox.com>
2020-05-19 11:32:58 -0700

    Adding usedBy to the module detail page

--- dashboard-fe/pages/api/graphql.js
+++ dashboard-fe/pages/api/graphql.js
@@ -15,6 +15,7 @@ const typeDefs = gql`
     name: String!
     file: String
     requires: [Override!]!
+    consumedBy: [Consume!]!
   }
 
   type Override {
@@ -90,6 +91,15 @@ const resolvers = {
       return requires.map((reqId) =>
         app.overrides.find(({ id }) => id === reqId)
       );
+    },
+    consumedBy({ applicationID, name }) {
+      return apps
+        .map(({ consumes }) => consumes)
+        .flat()
+        .filter(
+          ({ applicationID: conApp, name: conName }) =>
+            conApp === applicationID && conName === name
+        );
     },
   },
   Override: {
