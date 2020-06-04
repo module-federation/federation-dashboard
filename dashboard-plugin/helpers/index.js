1370e1fb1c338986512775708c900714a69def89
Andrew James <13269277+andrew-t-james@users.noreply.github.com>
2020-06-04 17:15:28 -0500

    WIP: Add tests for dashboard plugin (#31)
    * Add single test for convertToGraph.js
    
    * Remove rawData file writes
    
    * Update test and add mock data

---
+++ dashboard-plugin/helpers/index.js
@@ -0,0 +1,69 @@
+function validateParams({
+  federationRemoteEntry,
+  topLevelPackage,
+  metadata,
+  modules,
+}) {
+  function objHasKeys(nestedObj, pathArr) {
+    return pathArr.reduce(
+      (obj, key) => (obj && obj[key] !== "undefined" ? obj[key] : undefined),
+      nestedObj
+    );
+  }
+
+  const hasLoc = objHasKeys(federationRemoteEntry, ["origins", "0", "loc"]);
+  const hasDependencies = objHasKeys(topLevelPackage, ["dependencies"]);
+  const hasDevDependencies = objHasKeys(topLevelPackage, ["devDependencies"]);
+  const hasOptionalDependencies = objHasKeys(topLevelPackage, [
+    "optionalDependencies",
+  ]);
+  const hasMetaDataSource = objHasKeys(metadata, ["source", "url"]);
+  const hasMetaDataRemote = objHasKeys(metadata, ["remote"]);
+
+  if (
+    typeof hasLoc === "undefined" ||
+    federationRemoteEntry.origins[0].loc === ""
+  ) {
+    throw new Error(
+      "federationRemoteEntry.origins[0].loc must be defined and have a value"
+    );
+  }
+
+  if ((modules && !modules.length) || typeof modules === "undefined") {
+    throw new Error("Modules must be defined and have length");
+  }
+
+  if (typeof hasDependencies === "undefined") {
+    throw new Error("topLevelPackage.dependencies must be defined");
+  }
+
+  if (typeof hasDevDependencies === "undefined") {
+    throw new Error("topLevelPackage.devDependencies must be defined");
+  }
+
+  if (typeof hasOptionalDependencies === "undefined") {
+    throw new Error("topLevelPackage.optionalDependencies must be defined");
+  }
+
+  for (let module of modules) {
+    if (typeof module.identifier === "undefined") {
+      throw new Error("module.identifier must be defined");
+    }
+    if (typeof module.reasons === "undefined") {
+      throw new Error("module.reasons must be defined");
+    }
+    if (typeof module.issuerName === "undefined") {
+      throw new Error("module.issuerName must be defined");
+    }
+  }
+
+  if (typeof hasMetaDataSource === "undefined") {
+    throw new Error("metadata.source.url must be defined");
+  }
+
+  if (typeof hasMetaDataRemote === "undefined") {
+    throw new Error("metadata.remote must be defined");
+  }
+}
+
+module.exports = { validateParams }
