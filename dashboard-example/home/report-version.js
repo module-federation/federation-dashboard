2982a2efdfccc280ca4dc764c8d8d174ad0c99b3
zackjackson <zackary.l.jackson@gmail.com>
2020-06-14 17:21:45 -0700

    Live versioning

---
+++ dashboard-example/home/report-version.js
@@ -0,0 +1,32 @@
+console.log();
+const http = require("http");
+
+const data = JSON.stringify({
+  version: require("./package.json").version,
+});
+
+const options = {
+  hostname: "localhost",
+  port: 3010,
+  path: "/write/home",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+};
+
+const req = http.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
