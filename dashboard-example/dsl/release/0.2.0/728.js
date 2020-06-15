2da891c6334c2d54367dd9661949f405a59807d2
Zack Jackson <zackary.l.jackson@gmail.com>
2020-06-14 21:20:31 -0700

    Write Mode working

--- dashboard-example/dsl/release/0.2.0/728.js
+++ dashboard-example/dsl/release/0.2.0/728.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp_dashboard_dsl=window.webpackJsonp_dashboard_dsl||[]).push([[728],{31728:(e,s,a)=>{"use strict";a.r(s),a.d(s,{default:()=>o});var d=a(10456),n=a.n(d),r=a(28722);const o=({children:e,...s})=>(alert("new version"),n().createElement(r.Carousel,s,e))}}]);
+(window.webpackJsonp_dashboard_dsl=window.webpackJsonp_dashboard_dsl||[]).push([[728],{31728:(e,s,a)=>{"use strict";a.r(s),a.d(s,{default:()=>o});var d=a(10456),n=a.n(d),r=a(28722);const o=({children:e,...s})=>(alert("úsing new version"),n().createElement(r.Carousel,s,e))}}]);
--- dashboard-example/home/src/index.js
+++ dashboard-example/home/src/index.js
@@ -1,26 +1,40 @@
+const injectScript = function (d, s, id, override) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  if (override && override.version) {
+    window[`remote_${override.name}`] = override.version;
+  }
+  js.onload = function () {
+    import("./bootstrap");
+  };
+  const src =
+    override && override.version
+      ? "http://localhost:3002/" + override.version + "/remoteEntry.js"
+      : "http://localhost:3002/remoteEntry.js";
+  js.src = src;
+  fjs.parentNode.insertBefore(js, fjs);
+};
 fetch("http://localhost:3010/")
   .then((res) => res.json())
   .then((versions) => {
     window.versions = versions;
     Object.assign(versions, { currentHost: "home" });
-    const promiseArray = [];
+    console.log(versions)
+    if (!versions.home.override.length) {
+      injectScript(document, "script", "federation-dynamic-remote");
+      return
+    }
     versions.home.override.forEach((override) => {
-      window[override] = null;
-      (function (d, s, id) {
-        var js,
-          fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-          return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.onload = function () {
-          import("./bootstrap");
-        };
-        js.src =
-          "http://localhost:3002/" + override.version + "/remoteEntry.js";
-        Object.assign(versions.home, { dsl: override.version });
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "federation-override-" + override.name);
+      injectScript(
+        document,
+        "script",
+        "federation-override-" + override.name,
+        override
+      );
     });
   });
