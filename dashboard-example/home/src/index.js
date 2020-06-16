facae6565f4bd566a2aef2a1f63c44f2d05b6d73
Zack Jackson <zackary.l.jackson@gmail.com>
2020-06-15 21:00:23 -0700

    implement promise chain for multiple versioned remotes

--- dashboard-example/home/src/index.js
+++ dashboard-example/home/src/index.js
@@ -1,40 +1,49 @@
 const injectScript = function (d, s, id, override) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  if (override && override.version) {
-    window[`remote_${override.name}`] = override.version;
-  }
-  js.onload = function () {
-    import("./bootstrap");
-  };
-  const src =
-    override && override.version
-      ? "http://localhost:3002/" + override.version + "/remoteEntry.js"
-      : "http://localhost:3002/remoteEntry.js";
-  js.src = src;
-  fjs.parentNode.insertBefore(js, fjs);
+  const promise = new Promise((resolve) => {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    if (override && override.version) {
+      window[`remote_${override.name}`] = override.version;
+    }
+    js.onload = function () {
+      resolve();
+    };
+    const src =
+      override && override.version
+        ? "http://localhost:3002/" + override.version + "/remoteEntry.js"
+        : "http://localhost:3002/remoteEntry.js";
+    js.src = src;
+    fjs.parentNode.insertBefore(js, fjs);
+  });
+
+  return promise;
 };
+
 fetch("http://localhost:3010/")
   .then((res) => res.json())
   .then((versions) => {
     window.versions = versions;
-    Object.assign(versions, { currentHost: "home" });
-    console.log(versions)
+    console.log(versions);
     if (!versions.home.override.length) {
-      injectScript(document, "script", "federation-dynamic-remote");
-      return
+      injectScript(document, "script", "federation-dynamic-remote").then(() => {
+        import("./bootstrap");
+      });
+      return;
     }
-    versions.home.override.forEach((override) => {
-      injectScript(
+    const allOverrides = versions.home.override.map((override) => {
+      return injectScript(
         document,
         "script",
         "federation-override-" + override.name,
         override
       );
+    });
+    Promise.all(allOverrides).then(() => {
+      import("./bootstrap");
     });
   });
