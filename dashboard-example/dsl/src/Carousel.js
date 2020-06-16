a43c2da8a241ad5900dba406b8be7ec82186fbd3
Zack Jackson <zackary.l.jackson@gmail.com>
2020-06-15 22:25:13 -0700

    publicpath at runtime

--- dashboard-example/dsl/src/Carousel.js
+++ dashboard-example/dsl/src/Carousel.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Carousel } from "antd";
 
 const MyCarousel = ({ children, ...props }) => {
-  return <Carousel {...props}>{children}</Carousel>;
+  return <div>  <h1>Using DSL Version 0.2.0</h1>  <Carousel {...props}>{children}</Carousel></div>;
 };
 
 export default MyCarousel;
