70662448226992f48425b5e722cfb2152a6e6085
Jack Herrington <jherr@pobox.com>
2020-06-17 13:11:21 -0700

    Adding arrow heads

--- dashboard-fe/components/ModuleUMLDiagram.tsx
+++ dashboard-fe/components/ModuleUMLDiagram.tsx
@@ -6,10 +6,147 @@ import createEngine, {
   DagreEngine,
   DiagramEngine,
   PathFindingLinkFactory,
+  DefaultLinkModel,
+  DefaultLinkWidget,
+  PointModel,
+  LinkWidget,
+  DefaultLinkFactory,
 } from "@projectstorm/react-diagrams";
 import * as React from "react";
 import { CanvasWidget } from "@projectstorm/react-canvas-core";
 import styled from "@emotion/styled";
+
+export class AdvancedLinkModel extends DefaultLinkModel {
+  constructor() {
+    super({
+      type: "advanced",
+      width: 4,
+    });
+    this.showArrow = false;
+  }
+}
+
+export class AdvancedPortModel extends DefaultPortModel {
+  createLinkModel(): AdvancedLinkModel | null {
+    return new AdvancedLinkModel();
+  }
+}
+
+const CustomLinkArrowWidget = (props) => {
+  const { point, previousPoint } = props;
+
+  const angle =
+    90 +
+    (Math.atan2(
+      point.getPosition().y - previousPoint.getPosition().y,
+      point.getPosition().x - previousPoint.getPosition().x
+    ) *
+      180) /
+      Math.PI;
+
+  //translate(50, -10),
+  return (
+    <g
+      className="arrow"
+      transform={
+        "translate(" +
+        point.getPosition().x +
+        ", " +
+        point.getPosition().y +
+        ")"
+      }
+    >
+      <g style={{ transform: "rotate(" + angle + "deg)" }}>
+        <g transform={"translate(0, -3)"}>
+          <polygon
+            points="0,10 8,30 -8,30"
+            fill={props.color}
+            onMouseLeave={() => {
+              this.setState({ selected: false });
+            }}
+            onMouseEnter={() => {
+              this.setState({ selected: true });
+            }}
+            data-id={point.getID()}
+            data-linkid={point.getLink().getID()}
+          ></polygon>
+        </g>
+      </g>
+    </g>
+  );
+};
+
+export class AdvancedLinkWidget extends DefaultLinkWidget {
+  generateArrow(point: PointModel, previousPoint: PointModel): JSX.Element {
+    return this.props.link.getOptions().showArrow ? (
+      <CustomLinkArrowWidget
+        key={point.getID()}
+        point={point as any}
+        previousPoint={previousPoint as any}
+        colorSelected={this.props.link.getOptions().selectedColor}
+        color={this.props.link.getOptions().color}
+      />
+    ) : null;
+  }
+
+  render() {
+    //ensure id is present for all points on the path
+    var points = this.props.link.getPoints();
+    var paths = [];
+    this.refPaths = [];
+
+    //draw the multiple anchors and complex line instead
+    for (let j = 0; j < points.length - 1; j++) {
+      paths.push(
+        this.generateLink(
+          LinkWidget.generateLinePath(points[j], points[j + 1]),
+          {
+            "data-linkid": this.props.link.getID(),
+            "data-point": j,
+            onMouseDown: (event: MouseEvent) => {
+              this.addPointToLink(event, j + 1);
+            },
+          },
+          j
+        )
+      );
+    }
+
+    for (let i = 1; i < points.length - 1; i++) {
+      paths.push(this.generatePoint(points[i]));
+    }
+
+    if (this.props.link.getTargetPort() !== null) {
+      paths.push(
+        this.generateArrow(points[points.length - 1], points[points.length - 2])
+      );
+    } else {
+      paths.push(this.generatePoint(points[points.length - 1]));
+    }
+
+    return (
+      <g data-default-link-test={this.props.link.getOptions().testName}>
+        {paths}
+      </g>
+    );
+  }
+}
+
+export class AdvancedLinkFactory extends DefaultLinkFactory {
+  constructor() {
+    super("advanced");
+  }
+
+  generateModel(): AdvancedLinkModel {
+    return new AdvancedLinkModel();
+  }
+
+  generateReactWidget(event): JSX.Element {
+    return (
+      <AdvancedLinkWidget link={event.model} diagramEngine={this.engine} />
+    );
+  }
+}
 
 export interface CanvasContainerProps {}
 
@@ -89,6 +226,7 @@ const LINK_SIZE_DEFAULT = 3;
 
 export default ({ applications }) => {
   let engine = createEngine();
+  engine.getLinkFactories().registerFactory(new AdvancedLinkFactory());
 
   let model = new DiagramModel();
 
@@ -119,12 +257,14 @@ export default ({ applications }) => {
                   : LINK_COLOR_UNSELECTED;
               l.getOptions().width =
                 sourceName === name || targetName.startsWith(name) ? 3 : 0;
+              l.getOptions().showArrow = true;
             });
           } else {
             nodes.forEach((n) => {
               n.getOptions().color = NODE_COLOR_DEFAULT;
             });
             links.forEach((l) => {
+              l.getOptions().showArrow = false;
               l.getOptions().color = LINK_COLOR_DEFAULT;
               l.getOptions().width = LINK_SIZE_DEFAULT;
             });
@@ -133,13 +273,13 @@ export default ({ applications }) => {
       },
     });
 
-    const port = new DefaultPortModel(true, name, "Application");
+    const port = new AdvancedPortModel(true, name, "Application");
     ports[name] = port;
     node.addPort(port);
 
     modules.forEach(({ name: moduleName }) => {
       const id = `${name}:${moduleName}`;
-      const port = new DefaultPortModel(true, id, moduleName);
+      const port = new AdvancedPortModel(true, id, moduleName);
       ports[id] = port;
       node.addPort(port);
     });
