6307f7d491b528e0013d67b5765f370fbe6642d5
Jack Herrington <jherr@pobox.com>
2020-06-24 17:54:26 -0700

    UML beauty updates

--- dashboard-fe/components/ModuleUMLDiagram.tsx
+++ dashboard-fe/components/ModuleUMLDiagram.tsx
@@ -6,10 +6,127 @@ import createEngine, {
   DagreEngine,
   DiagramEngine,
   PathFindingLinkFactory,
+  AbstractReactFactory,
+  DefaultPortLabel,
+  DefaultNodeFactory,
 } from "@projectstorm/react-diagrams";
 import { CanvasWidget } from "@projectstorm/react-canvas-core";
 import styled from "@emotion/styled";
+
 import store from "../src/store";
+
+namespace S {
+  export const Node = styled.div<{ background: string; selected: boolean }>`
+    background-color: ${(p) => p.background};
+    border-radius: 5px;
+    font-family: sans-serif;
+    color: white;
+    border: solid 2px black;
+    overflow: visible;
+    font-size: 14px;
+    border: solid 2px ${(p) => (p.selected ? "rgb(0,192,255)" : "black")};
+  `;
+
+  export const Title = styled.div`
+    background: rgba(0, 0, 0, 0.6);
+    display: flex;
+    white-space: nowrap;
+    justify-items: center;
+  `;
+
+  export const TitleName = styled.div`
+    flex-grow: 1;
+    padding: 5px 5px;
+    font-size: 15pt;
+    font-weight: bold;
+    text-align: center;
+  `;
+
+  export const InPortItem = styled.div`
+    margin-top: 3px;
+    margin-bottom: 3px;
+  `;
+
+  export const OutPortItem = styled.div`
+    opacity: 0;
+  `;
+
+  export const Ports = styled.div`
+    display: flex;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2));
+  `;
+
+  export const PortsContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    &:first-of-type {
+      margin-right: 10px;
+    }
+    &:only-child {
+      margin-right: 0px;
+    }
+  `;
+}
+
+export interface DefaultNodeProps {
+  node: DefaultNodeModel;
+  engine: DiagramEngine;
+}
+
+export class NicerNodeWidget extends React.Component<DefaultNodeProps> {
+  generateInPort = (port) => {
+    return (
+      <S.InPortItem>
+        <DefaultPortLabel
+          engine={this.props.engine}
+          port={port}
+          key={port.getID()}
+        />
+      </S.InPortItem>
+    );
+  };
+
+  generateOutPort = (port) => {
+    return (
+      <S.OutPortItem>
+        <DefaultPortLabel
+          engine={this.props.engine}
+          port={port}
+          key={port.getID()}
+        />
+      </S.OutPortItem>
+    );
+  };
+
+  render() {
+    return (
+      <S.Node
+        data-default-node-name={this.props.node.getOptions().name}
+        selected={this.props.node.isSelected()}
+        background={this.props.node.getOptions().color}
+      >
+        <S.Title>
+          <S.TitleName>{this.props.node.getOptions().name}</S.TitleName>
+        </S.Title>
+        <S.Ports>
+          <S.PortsContainer>
+            {this.props.node.getInPorts().map(this.generateInPort)}
+          </S.PortsContainer>
+          <S.PortsContainer>
+            {this.props.node.getOutPorts().map(this.generateOutPort)}
+          </S.PortsContainer>
+        </S.Ports>
+      </S.Node>
+    );
+  }
+}
+
+export class NicerNodeFactory extends DefaultNodeFactory {
+  generateReactWidget(event): JSX.Element {
+    return <NicerNodeWidget engine={this.engine} node={event.model} />;
+  }
+}
 
 export interface CanvasContainerProps {}
 
@@ -90,6 +207,9 @@ class ModuleUMLDiagram extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    this.engine = createEngine();
+    this.engine.getNodeFactories().registerFactory(new NicerNodeFactory());
+
     this.model = new DiagramModel();
 
     const nodes: NodeModel[] = [];
@@ -140,7 +260,7 @@ class ModuleUMLDiagram extends React.PureComponent {
         },
       });
 
-      ports[name] = node.addOutPort(name);
+      ports[name] = node.addOutPort("");
       ports[name].getOptions().id = name;
 
       modules.forEach(({ name: moduleName }) => {
@@ -174,7 +294,6 @@ class ModuleUMLDiagram extends React.PureComponent {
     });
 
     this.model.setLocked(true);
-    this.engine = createEngine();
     this.engine.setModel(this.model);
   }
 
