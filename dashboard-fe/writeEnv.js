60ff8020b490119c070e5d04b7a68307c658f5b6
zackjackson <zackary.l.jackson@gmail.com>
2020-06-25 21:06:42 -0700

    stringify env vars

--- dashboard-fe/writeEnv.js
+++ dashboard-fe/writeEnv.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 fs.writeFileSync(
   ".env",
   [
-    "auth0_client_secret=" + process.env.AUTH0_CLIENT_SECRET,
-    "auth0_session_secret=" + process.env.SESSION_COOKIE_SECRET,
+    "auth0_client_secret=" + JSON.stringify(process.env.AUTH0_CLIENT_SECRET),
+    "auth0_session_secret=" + JSON.stringify(process.env.SESSION_COOKIE_SECRET),
   ].join("\n")
 );
