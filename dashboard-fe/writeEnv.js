b89bad1a9a4ca9fc97656eefa080bdcc567a6635
zackjackson <zackary.l.jackson@gmail.com>
2020-06-25 21:19:39 -0700

    change buildargs permissions

--- dashboard-fe/writeEnv.js
+++ dashboard-fe/writeEnv.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 fs.writeFileSync(
   ".env",
   [
-    "auth0_client_secret=" + JSON.stringify(process.env.AUTH0_CLIENT_SECRET),
-    "auth0_session_secret=" + JSON.stringify(process.env.SESSION_COOKIE_SECRET),
+    "AUTH0_CLIENT_SECRET=" + JSON.stringify(process.env.AUTH0_CLIENT_SECRET),
+    "AUTH0_CLIENT_SECRET=" + JSON.stringify(process.env.SESSION_COOKIE_SECRET),
   ].join("\n")
 );
