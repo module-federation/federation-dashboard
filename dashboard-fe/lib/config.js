093e75e6a5ab04a15c2be52a32bb539ebffcb1e4
zackjackson <zackary.l.jackson@gmail.com>
2020-06-25 22:38:39 -0700

    testing mock keys

--- dashboard-fe/lib/config.js
+++ dashboard-fe/lib/config.js
@@ -2,6 +2,7 @@ if (typeof window === "undefined") {
   /**
    * Settings exposed to the server.
    */
+
   module.exports = {
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
@@ -9,7 +10,10 @@ if (typeof window === "undefined") {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     REDIRECT_URI: process.env.REDIRECT_URI,
     POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
-    SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
+    SESSION_COOKIE_SECRET: new Buffer(
+      process.env.SESSION_COOKIE_SECRET,
+      "base64"
+    ).toString("ascii"),
     SESSION_COOKIE_LIFETIME: 60 * 60 * 8,
   };
 } else {
