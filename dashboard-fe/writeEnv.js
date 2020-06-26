9920d2d1f0b893951cf40dd8eaa79da508e26f87
zackjackson <zackary.l.jackson@gmail.com>
2020-06-25 21:36:08 -0700

    change buildargs permissions

--- dashboard-fe/writeEnv.js
+++ dashboard-fe/writeEnv.js
@@ -6,3 +6,4 @@ fs.writeFileSync(
     "AUTH0_CLIENT_SECRET=" + JSON.stringify(process.env.SESSION_COOKIE_SECRET),
   ].join("\n")
 );
+console.log(fs.readFileSync("./.env").toString().split("").reverse());
