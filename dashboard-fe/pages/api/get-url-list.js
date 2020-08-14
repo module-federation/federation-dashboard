97869333c7adfec843837dd3820bfe7572fa2a50
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-14 14:39:31 -0700

    adding cache

--- dashboard-fe/pages/api/get-url-list.js
+++ dashboard-fe/pages/api/get-url-list.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   const url = isProd
     ? "https://lighthouse-perf-compare.vercel.app/urls.json"
     : "http://localhost:3000/urls.json";
-  console.log(url);
+
   const json = await fetch(url).then((res) => {
     return res.json();
   });
