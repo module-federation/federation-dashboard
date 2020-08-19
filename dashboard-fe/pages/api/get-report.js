5993d1bca25f0d26d547ec8dc51c684a6109f203
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2020-08-19 12:44:36 -0700

    linting

--- dashboard-fe/pages/api/get-report.js
+++ dashboard-fe/pages/api/get-report.js
@@ -3,7 +3,9 @@ import path from "path";
 
 export default async (req, res) => {
   res.statusCode = 200;
+
   const safePath = req.query.report.split("/").slice(-1)[0];
+  console.log("get Report", safePath);
   const hostname = "http://" + req.headers.host + "/";
   const url = hostname + path.join("reports", safePath, "scatter.json");
   const json = await fetch(url).then(res => res.json());
