8a6693067e759270bc01258174e80f836f96f469
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-21 13:38:00 -0700

    memoize charts

--- dashboard-fe/pages/performance/reports/[report].js
+++ dashboard-fe/pages/performance/reports/[report].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useRef,createRef,useEffect,useState} from "react";
 import dynamic from "next/dynamic";
 import {
   generateScatterChartData,
@@ -21,31 +21,186 @@ const CanvasJSChart = dynamic(
   { ssr: false }
 );
 
+const WhiskerChart = React.memo((props) => {
+  const [ready,setReady] = useState(false)
+  useEffect(()=>{
+    setReady(true)
+  },[])
+  if(!ready) {
+    return null
+  }
+  const whiskerChartOptions = {
+    theme: "dark2",
+    height: typeof window === "object" ? window.innerHeight : null,
+    interactivityEnabled: true,
+    zoomEnabled: true,
+    zoomType: "y",
+    title: {
+      text: "Performance chart",
+    },
+    axisX: {
+      title: "Metric Type",
+    },
+    axisY: {
+      scaleBreaks: {
+        autoCalculate: true, // change to false
+      },
+      includeZero: false,
+      title: "Timing",
+      suffix: "ms",
+      logarithmic: true,
+      logarithmBase: 2,
+    },
+    legend: {
+      cursor: "pointer",
+      itemclick: props.toggleDataSeries,
+    },
+
+    data: props.whiskerChartData,
+  };
+  return (
+      <CanvasJSChart
+          options={whiskerChartOptions}
+          onRef={createRef}
+      />
+  )
+})
+const ScatterChart = React.memo((props)=>{
+  const [ready,setReady] = useState(false)
+  useEffect(()=>{
+    setReady(true)
+  },[])
+  if(!ready) {
+    return null
+  }
+  const scatterChartOptions = {
+    height: typeof window === "object" ? window.innerHeight : null,
+    theme: "dark2",
+    zoomEnabled: true,
+    zoomType: "x",
+    title: {
+      text: "Scatter Chart",
+    },
+    axisX: {
+      title: "Metric Type",
+    },
+    axisY: {
+      title: "Timing",
+      suffix: "ms",
+      includeZero: false,
+      logarithmic: true,
+    },
+    legend: {
+      cursor: "pointer",
+      itemclick: props.toggleDataSeries,
+    },
+
+    data: props.scatterChartData,
+  };
+  return (
+      <CanvasJSChart
+          options={scatterChartOptions}
+          onRef={createRef}
+      />
+  )
+})
+const MultiSeriesChart = React.memo((props)=>{
+  const [ready,setReady] = useState(false)
+  useEffect(()=>{
+    setReady(true)
+  },[])
+  if(!ready) {
+    return null
+  }
+  const multiSeriesChartOptions = {
+    theme: "dark2",
+    animationEnabled: true,
+    title: {
+      text: "Multi Series Chart - Medians",
+    },
+    axisY: {
+      title: "Metric Type",
+      includeZero: true,
+    },
+    legend: {
+      cursor: "pointer",
+      itemclick: props.toggleDataSeries,
+    },
+    toolTip: {
+      shared: true,
+      // content: toolTipFormatter
+    },
+    data: props.multiSeriesChartData,
+  };
+
+  return (
+      <CanvasJSChart
+          options={multiSeriesChartOptions}
+          onRef={createRef}
+      />
+  )
+})
+const TimeSeriesChart = React.memo((props)=>{
+  const [data,setData] = useState(false)
+  useEffect(()=>{
+      const { query } = props;
+   const {CanvasJS }= require("canvasjs-react-charts");
+      fetch(
+          hostname + "api/get-timeseries?report=" + query.report
+      ).then((res) => res.json()).then((timeSeriesData)=>{
+        const timeSeriesScatterData = generateTimeSeriesScatterChartData(timeSeriesData);
+
+        const timeSeriesScatterChartOptions = {
+          height: typeof window === "object" ? window.innerHeight : null,
+          theme: "dark2",
+          title: {
+            text: "Scatter Chart",
+          },
+          axisX: {
+            title: "Date",
+            labelFormatter: function (e) {
+                return CanvasJS.formatDate(e.value, "DD MMM")
+            },
+          },
+          axisY: {
+            title: "Timing",
+            suffix: "ms",
+            includeZero: false,
+            logarithmic: true,
+            interval: 0.2,
+          },
+          legend: {
+            cursor: "pointer",
+            itemclick: props.toggleDataSeries,
+          },
+
+          data: timeSeriesScatterData,
+        };
+
+        setData(timeSeriesScatterChartOptions)
+      })
+  },[])
+  if(!data) {
+    return null
+  }
+
+
+  return (
+      <CanvasJSChart
+          options={data}
+          onRef={createRef}
+      />
+  )
+})
+
 class Report extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       inputValue: "",
-      timeSeriesScatterData: props.timeSeriesScatterData,
-      mounted:true
     };
     this.toggleDataSeries = this.toggleDataSeries.bind(this);
   }
-  // componentDidMount = async () => {
-  //   const { query } = this.props;
-  //   window.CanvasJS = require("canvasjs-react-charts");
-  //   console.log(window.CanvasJS);
-  //   const timeSeriesData = await fetch(
-  //     hostname + "api/get-timeseries?report=" + query.report
-  //   ).then((res) => res.json());
-  //
-  //   const scatterData = generateTimeSeriesScatterChartData(timeSeriesData);
-  //
-  //   this.setState({
-  //     timeSeriesScatterData: scatterData,
-  //     mounted:true
-  //   });
-  // };
 
   toggleDataSeries(e) {
     if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
@@ -53,7 +208,6 @@ class Report extends React.Component {
     } else {
       e.dataSeries.visible = true;
     }
-    this.chart.render();
     this.chart2.render();
     this.chart3.render();
     this.chart4.render();
@@ -90,123 +244,7 @@ class Report extends React.Component {
   };
 
   render() {
-    const whiskerChartOptions = {
-      theme: "dark2",
-      height: typeof window === "object" ? window.innerHeight : null,
-      interactivityEnabled: true,
-      zoomEnabled: true,
-      zoomType: "y",
-      title: {
-        text: "Performance chart",
-      },
-      axisX: {
-        title: "Metric Type",
-      },
-      axisY: {
-        scaleBreaks: {
-          autoCalculate: true, // change to false
-        },
-        includeZero: false,
-        title: "Timing",
-        suffix: "ms",
-        logarithmic: true,
-        logarithmBase: 2,
-      },
-      legend: {
-        cursor: "pointer",
-        itemclick: this.toggleDataSeries,
-      },
 
-      data: this.props.whiskerChartData,
-    };
-    const scatterChartOptions = {
-      height: typeof window === "object" ? window.innerHeight : null,
-      theme: "dark2",
-      zoomEnabled: true,
-      zoomType: "x",
-      title: {
-        text: "Scatter Chart",
-      },
-      axisX: {
-        title: "Metric Type",
-      },
-      axisY: {
-        title: "Timing",
-        suffix: "ms",
-        includeZero: false,
-        logarithmic: true,
-      },
-      legend: {
-        cursor: "pointer",
-        itemclick: this.toggleDataSeries,
-      },
-
-      data: this.props.scatterChartData,
-    };
-
-    const timeSeriesScatterChartOptions = {
-      height: typeof window === "object" ? window.innerHeight : null,
-      theme: "dark2",
-      title: {
-        text: "Scatter Chart",
-      },
-      axisX: {
-        title: "Date",
-        labelFormatter: function (e) {
-          if(process.browser && window.CanvasJS) {
-            const {CanvasJS} = window.CanvasJS
-            return CanvasJS.formatDate(e.value, "DD MMM")
-          }
-          return e.value
-        },
-      },
-      axisY: {
-        title: "Timing",
-        suffix: "ms",
-        includeZero: false,
-        logarithmic: true,
-        interval: 0.2,
-      },
-      legend: {
-        cursor: "pointer",
-        itemclick: this.toggleDataSeries,
-      },
-
-      data: this.state.timeSeriesScatterData,
-    };
-    const multiSeriesChartOptions = {
-      theme: "dark2",
-      animationEnabled: true,
-      title: {
-        text: "Multi Series Chart - Medians",
-      },
-      axisY: {
-        title: "Metric Type",
-        includeZero: true,
-      },
-      legend: {
-        cursor: "pointer",
-        itemclick: this.toggleDataSeries,
-      },
-      toolTip: {
-        shared: true,
-        // content: toolTipFormatter
-      },
-      data: this.props.multiSeriesChartData,
-    };
-    if(!this.state.mounted) {
-      return (
-          <div>
-            <Form
-                appKeys={this.props.appKeys}
-                onSubmit={this._handleSubmit}
-                onDelete={this.onDelete}
-                value={this.state.inputValue}
-                onChange={(e) => this.setState({ inputValue: e.target.value })}
-            />
-          </div>
-      )
-    }
     return (
       <>
         <div>
@@ -219,22 +257,10 @@ class Report extends React.Component {
           />
         </div>
         <div>
-          <CanvasJSChart
-            options={whiskerChartOptions}
-            onRef={(ref) => (this.chart = ref)}
-          />
-          <CanvasJSChart
-            options={scatterChartOptions}
-            onRef={(ref) => (this.chart2 = ref)}
-          />
-          <CanvasJSChart
-            options={multiSeriesChartOptions}
-            onRef={(ref) => (this.chart3 = ref)}
-          />
-          <CanvasJSChart
-            options={timeSeriesScatterChartOptions}
-            onRef={(ref) => (this.chart4 = ref)}
-          />
+         <WhiskerChart whiskerChartData={this.props.whiskerChartData} toggleDataSeries={this.toggleDataSeries}/>
+         <ScatterChart scatterChartData={this.props.scatterChartData} toggleDataSeries={this.toggleDataSeries}/>
+         <MultiSeriesChart multiSeriesChartData={this.props.multiSeriesChartData} toggleDataSeries={this.toggleDataSeries}/>
+         <TimeSeriesChart query={this.props.query} multiSeriesChartData={this.props.multiSeriesChartData} toggleDataSeries={this.toggleDataSeries}/>
         </div>
       </>
     );
@@ -245,18 +271,18 @@ Report.getInitialProps = async ({ query }) => {
   const { meta, ...report } = await fetch(
     hostname + "api/get-report?report=" + query.report
   ).then((res) => res.json());
-  const timeSeriesData = await fetch(
-    hostname + "api/get-timeseries?report=" + query.report
-  ).then((res) => res.json());
-
-  const scatterData = generateTimeSeriesScatterChartData(timeSeriesData);
+  // const timeSeriesData = await fetch(
+  //   hostname + "api/get-timeseries?report=" + query.report
+  // ).then((res) => res.json());
+  //
+  // const scatterData = generateTimeSeriesScatterChartData(timeSeriesData);
   return {
     scatterChartData: generateScatterChartData(report),
     whiskerChartData: generateWhiskerChartData(report),
     multiSeriesChartData: generateMultiSeriesChartData(report),
     meta,
     appKeys: Object.keys(report),
-    timeSeriesScatterData: scatterData,
+    // timeSeriesScatterData: scatterData,
     query,
   };
 };
