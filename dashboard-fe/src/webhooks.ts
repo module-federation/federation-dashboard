2d316f5a7f8039f76f4be0fcf5b9fe2a277fbcec
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-26 13:12:09 -0700

    fix: typo in page name

--- dashboard-fe/src/webhooks.ts
+++ dashboard-fe/src/webhooks.ts
@@ -1,9 +1,12 @@
 import bus from "./event-bus";
 import dbDriver from "./database/drivers";
 import fetch from "node-fetch";
+// import "../lighthouse/add-url"
 
 const hookSusbscriber = async (type, payload) => {
-  const settings = await dbDriver.siteSettings_get();
+    console.log(type,payload)
+
+    const settings = await dbDriver.siteSettings_get();
   return Promise.all(
     settings.webhooks
       .filter(({ event }) => event === type)
