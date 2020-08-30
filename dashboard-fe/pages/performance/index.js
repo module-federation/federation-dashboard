4906dfebe671a1786734d42b8ec70072d3680ec5
Zack Jackson <zackary.l.jackson@gmail.com>
2020-08-30 12:23:02 -0700

    fixed individual re-run button

--- dashboard-fe/pages/performance/index.js
+++ dashboard-fe/pages/performance/index.js
@@ -9,7 +9,7 @@ import { useMutation, useQuery } from "@apollo/react-hooks";
 import store from "../../src/store";
 import {
   GET_HEAD_VERSION,
-  SET_REMOTE_VERSION
+  SET_REMOTE_VERSION,
 } from "../../components/application/CurrentVersion";
 
 import { makeIDfromURL, removeMeta } from "../../lighthouse/utils.js";
@@ -52,8 +52,8 @@ const Performance = ({ linkList }) => {
 
   const { data } = useQuery(GET_TRACKED, {
     variables: {
-      group: "default"
-    }
+      group: "default",
+    },
   });
 
   React.useEffect(() => {
@@ -65,12 +65,12 @@ const Performance = ({ linkList }) => {
 
   const [setUrl] = useMutation(ADD_URL);
 
-  const _handleSubmit = e => {
+  const _handleSubmit = (e) => {
     if (e) e.preventDefault();
     if (inputValue === "") return alert("URL is required");
 
     const newArr = todos.slice();
-    const valueExists = todos.find(item => {
+    const valueExists = todos.find((item) => {
       return item.url === inputValue;
     });
     if (!valueExists) {
@@ -80,9 +80,9 @@ const Performance = ({ linkList }) => {
           {
             name: inputName,
             new: true,
-            search: makeIDfromURL(inputValue).search
-          }
-        ]
+            search: makeIDfromURL(inputValue).search,
+          },
+        ],
       });
       setTodos(newArr);
       setInputValue("");
@@ -91,9 +91,9 @@ const Performance = ({ linkList }) => {
       setUrl({
         variables: {
           settings: {
-            trackedURLs: newArr
-          }
-        }
+            trackedURLs: newArr,
+          },
+        },
       });
       // fetch("/api/add-url", { method: "POST", body: null });
       // fetch("/api/add-url", { method: "POST", body: JSON.stringify(newArr) });
@@ -112,18 +112,18 @@ const Performance = ({ linkList }) => {
     setUrl({
       variables: {
         settings: {
-          trackedURLs: newArr
-        }
-      }
+          trackedURLs: newArr,
+        },
+      },
     });
 
     // fetch("/api/add-url", { method: "POST", body: null });
     // fetch("/api/add-url", { method: "POST", body: JSON.stringify(newArr) });
   };
 
-  const _handleBntReRun = index => {
+  const _handleBntReRun = (index) => {
     const newArr = todos.slice();
-    const updated = newArr[index].variants.map(variant => {
+    const updated = newArr[index].variants.map((variant) => {
       if (variant.name === "Latest") {
         variant.new = true;
       }
@@ -134,9 +134,9 @@ const Performance = ({ linkList }) => {
     setUrl({
       variables: {
         settings: {
-          trackedURLs: newArr
-        }
-      }
+          trackedURLs: newArr,
+        },
+      },
     });
     // fetch("/api/add-url", { method: "POST", body: null });
     // fetch("/api/add-url", { method: "POST", body: JSON.stringify(newArr) });
@@ -144,7 +144,7 @@ const Performance = ({ linkList }) => {
 
   const reRunAllTests = ({ type, index }) => {
     const toRerun = todos.reduce((acc, item) => {
-      const updated = item.variants.map(variant => {
+      const updated = item.variants.map((variant) => {
         if (variant.name === "Latest") {
           variant.new = true;
         }
@@ -157,9 +157,9 @@ const Performance = ({ linkList }) => {
     setUrl({
       variables: {
         settings: {
-          trackedURLs: toRerun
-        }
-      }
+          trackedURLs: toRerun,
+        },
+      },
     });
     setTodos(toRerun);
 
@@ -184,8 +184,8 @@ const Performance = ({ linkList }) => {
         reRunAllTests={reRunAllTests}
         value={inputValue}
         name={inputName}
-        onChange={e => setInputValue(e.target.value)}
-        onChangeName={e => setInputNameValue(e.target.value)}
+        onChange={(e) => setInputValue(e.target.value)}
+        onChangeName={(e) => setInputNameValue(e.target.value)}
       />
       <List>
         {todos &&
@@ -201,8 +201,7 @@ const Performance = ({ linkList }) => {
     </Fragment>
   );
 };
-Performance.getInitialProps = async (ctx)=>{
-  console.log(ctx)
-  return {}
-}
+Performance.getInitialProps = async (ctx) => {
+  return {};
+};
 export default Performance;
