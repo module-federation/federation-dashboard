87b5070fa664d6e5ad54afdcbd2666be21ed41ab
Zack Jackson <zackary.l.jackson@gmail.com>
2020-09-02 14:25:00 -0700

    fix duplicate urlsya

--- dashboard-fe/components/FormVarient.js
+++ dashboard-fe/components/FormVarient.js
@@ -3,13 +3,20 @@ import { Delete } from "@material-ui/icons";
 import { List, Button, ListItem, IconButton } from "@material-ui/core";
 
 export default function Form(props) {
+  const { values } = props;
   return (
     <form onSubmit={props.onSubmit} style={{ paddingLeft: 40, marginTop: 16 }}>
       <input
         type="text"
-        value={props.value}
-        onChange={props.onChange}
+        value={values.inputValue}
+        onChange={(e) => props.onChange({ inputValue: e.target.value })}
         placeholder="Variation Name"
+      />
+      <input
+        type="text"
+        value={values.queryValue}
+        onChange={(e) => props.onChange({ queryValue: e.target.value })}
+        placeholder="query"
       />
       <Button variant="contained" type="submit">
         Add Varient
