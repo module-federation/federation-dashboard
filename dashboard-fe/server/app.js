dd26ec71c4efe982be2baf3566bea6af2a4f21b2
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2020-10-10 20:17:51 -0700

    data update, increase parallel processing

--- dashboard-fe/server/app.js
+++ dashboard-fe/server/app.js
@@ -7,17 +7,33 @@ const cookieParser = require("cookie-parser");
 const ipfilter = require("express-ipfilter").IpFilter;
 const createNextRouteHandler = require("./lib/create-next-route-handler");
 const ipaddr = require("ipaddr.js");
-const isEqual = require("lodash/isEqual")
+const isEqual = require("lodash/isEqual");
+const ip = require("ip");
+const requestIp = require("request-ip");
 module.exports = ({ nextRoutesHandler }) => {
   const app = express();
   const routesHandler = nextRoutesHandler || createNextRouteHandler({ log });
   app.use(cookieParser());
-  if (process.env.IP_WHITELIST) {
-    const ips = process.env.IP_WHITELIST.split(" ").map((address)=>{
-      return address
-    });
+  app.use(requestIp.mw());
 
-    app.use(ipfilter(ips, { mode: "allow" }));
+  if (process.env.IP_WHITELIST) {
+    const ips = process.env.IP_WHITELIST.split(" ");
+
+    ips.push(ip.address());
+
+    app.use((req, res, next) => {
+
+      const parsedIP = ips.reduce((acc, add) => {
+        if (req.clientIp.includes(add)) {
+          acc.push(req.clientIp);
+          return acc;
+        }
+        acc.push(add);
+        return acc;
+      }, []);
+
+      ipfilter(parsedIP, { mode: "allow" })(req, res, next);
+    });
   }
   app.use(routesHandler);
 
