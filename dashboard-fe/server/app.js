e64168f7dca28cf35fd02c4b085d4e6588c34a21
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-16 11:43:37 -0700

    fix: fixing db errors (#150)

--- dashboard-fe/server/app.js
+++ dashboard-fe/server/app.js
@@ -6,15 +6,35 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const ipfilter = require("express-ipfilter").IpFilter;
 const createNextRouteHandler = require("./lib/create-next-route-handler");
-
+const ipaddr = require("ipaddr.js");
+const isEqual = require("lodash/isEqual");
+const ip = require("ip");
+const requestIp = require("request-ip");
 module.exports = ({ nextRoutesHandler }) => {
   const app = express();
   const routesHandler = nextRoutesHandler || createNextRouteHandler({ log });
-
   app.use(cookieParser());
-  const ips = ["34.100.90.12"];
+  app.use(requestIp.mw());
 
-  app.use(ipfilter(ips));
+  if (process.env.IP_WHITELIST) {
+    const ips = process.env.IP_WHITELIST.split(" ");
+
+    ips.push(ip.address());
+
+    app.use((req, res, next) => {
+
+      const parsedIP = ips.reduce((acc, add) => {
+        if (req.clientIp.includes(add)) {
+          acc.push(req.clientIp);
+          return acc;
+        }
+        acc.push(add);
+        return acc;
+      }, []);
+
+      ipfilter(parsedIP, { mode: "allow" })(req, res, next);
+    });
+  }
   app.use(routesHandler);
 
   return app;
