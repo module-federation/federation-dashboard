7b803f8defaafaf96cfa07833c5b84f079dc0cdf
Zack Jackson <zackary.l.jackson@gmail.com>
2021-01-19 12:57:31 -0800

    auto token generation (#225)
    Co-authored-by: Jack Herrington <jherr@pobox.com>

--- dashboard-example/nav/webpack.config.js
+++ dashboard-example/nav/webpack.config.js
@@ -80,7 +80,8 @@ module.exports = {
     new DashboardPlugin({
       publishVersion: require("./package.json").version,
       filename: "dashboard.json",
-      dashboardURL: "http://localhost:3000/api/update",
+      dashboardURL:
+        "http://localhost:3000/api/update?token=29f387e1-a00d-46ea-9fd6-02ca5e97449c",
       metadata: {
         source: {
           url:
