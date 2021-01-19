9f01a19ba97b6461f5b672c3d26857101c08f0d2
Zack Jackson <zackary.l.jackson@gmail.com>
2021-01-19 14:28:18 -0800

    cleanup

--- dashboard-example/dsl/src/Button.js
+++ dashboard-example/dsl/src/Button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "antd";
-import { button } from "./button.css";
 console.log(
   "REMOTE FEDERATION MANAGEMENT: using remote version: " +
     require("../package.json").version
