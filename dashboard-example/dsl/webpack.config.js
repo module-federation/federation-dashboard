34d5ca49f8a478957bc05c25c2acafecc8a40b56
Zack Jackson <zackary.l.jackson@gmail.com>
2021-01-19 16:47:48 -0800

    Token generator (#228)
    Co-authored-by: Jack Herrington <jherr@pobox.com>

--- dashboard-example/dsl/webpack.config.js
+++ dashboard-example/dsl/webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
           },
           {
             loader: "css-loader",
+            options: {
+              modules: true,
+            },
           },
           {
             loader: "less-loader",
