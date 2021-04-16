9417c369f698a8a958745417dda6c5ee6e57b591
Zack Jackson <zackary.l.jackson@gmail.com>
2021-04-16 16:53:01 -0700

    adding catch all

---
+++ dashboard-fe/pages/[...slug].js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+import store from "../src/store";
+
+function Profile(props) {
+  console.log(props);
+  return (
+    <Layout>
+      <h1>Catch All</h1>
+    </Layout>
+  );
+}
+
+export default Profile
