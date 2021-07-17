6a9d5a732eb0047f17b5f7353f0282e2493e7900
Zack Jackson <zackary.l.jackson@gmail.com>
2021-07-17 15:28:34 -0700

    fix TS errors

--- dashboard-fe/next-env.d.ts
+++ dashboard-fe/next-env.d.ts
@@ -4,3 +4,6 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
