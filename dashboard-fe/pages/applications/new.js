9d0cfe3bad86dec2a27c801aa003e5f54731b967
Zack Jackson <zackary.l.jackson@gmail.com>
2021-09-12 01:33:00 -0700

    Adding dashboard plugin template to setup (#378)
    * Adding dashboard plugin template to setup
    
    * fixing get apps query on new page
    
    adding dashboard plugin boilerlpate
    
    * get remote from metadata

--- dashboard-fe/pages/applications/new.js
+++ dashboard-fe/pages/applications/new.js
@@ -20,7 +20,6 @@ import { observer } from "mobx-react";
 import _ from "lodash";
 
 import store from "../../src/store";
-import withAuth from "../../components/with-auth";
 
 const GET_APPS = gql`
   query ($group: String!, $environment: String!) {
@@ -28,8 +27,14 @@ const GET_APPS = gql`
       applications {
         id
         name
+        metadata {
+          name
+          value
+        }
         versions(latest: true, environment: $environment) {
-          remote
+          remotes {
+            name
+          }
         }
       }
     }
@@ -64,6 +69,7 @@ const NewApp = () => {
       automaticFederation: true,
     },
   });
+
   const [applications, applicationsSet] = React.useState([]);
   const { data } = useQuery(GET_APPS, {
     variables: {
@@ -71,17 +77,19 @@ const NewApp = () => {
       environment: store.environment,
     },
   });
+  console.log(data);
   const classes = useStyles();
 
   const externalApplications = _.get(data, "groups[0].applications", []);
 
   const scriptTags = applications
-    .map(
-      (n) =>
-        `<script src="${
-          externalApplications.find(({ name }) => name === n).versions[0].remote
-        }"></script>`
-    )
+    .map((n) => {
+      return `<script data-webpack="${n}" src="${
+        externalApplications
+          .find(({ name }) => name === n)
+          ?.metadata?.find(({ name }) => name === "remote")?.value
+      }"></script>`;
+    })
     .join("\n");
 
   const remotesCode =
@@ -153,30 +161,56 @@ const NewApp = () => {
   );
   const boilerplate = Template.asString([
     'const { ModuleFederationPlugin } = require("webpack").container;',
+    'const DashboardPlugin = require("@module-federation/dashboard-plugin");',
     automaticPreamble,
   ]).replace(/^\t/gm, "  ");
 
+  const dashboardPlugin = Template.asString(
+    Template.indent([
+      `new DashboardPlugin(`,
+      Template.indent([
+        `publishVersion: require("./package.json").version`,
+        `dashboardURL: "https://federation-dashboard-alpha.vercel.app/api/update?token={writeToken}"`,
+        `name: "${watch("name")}",`,
+        `metadata: {`,
+        Template.indent([`remote: "http://localhost:3001/remoteEntry.js",`]),
+        `}`,
+      ]),
+      `)`,
+    ])
+  ).replace(/\t/gm, "  ");
   const codeSample = Template.asString([pluginTemplate]).replace(/\t/g, "  ");
   return (
     <Layout>
       <Head>
         <title>New Federated Module Application</title>
       </Head>
-      {data && (
-        <Grid container className={classes.container} spacing={3}>
-          <Grid item xs={3}>
-            <Typography variant="h5">Export Modules</Typography>
+      <Grid container className={classes.container} spacing={3}>
+        <Grid item xs={3}>
+          <Typography variant="h5">Export Modules</Typography>
+          <TextField
+            name="name"
+            label="Application Name"
+            fullWidth
+            variant="outlined"
+            inputRef={register({ required: true })}
+            className={classes.textField}
+          />
+          <TextField
+            name="files"
+            label="Files to export"
+            helperText="comma seperated"
+            variant="outlined"
+            fullWidth
+            multiline
+            inputRef={register()}
+            className={classes.textField}
+          />
+
+          {!watch("automaticFederation") && (
             <TextField
-              name="name"
-              label="Application Name"
-              fullWidth
-              variant="outlined"
-              inputRef={register({ required: true })}
-              className={classes.textField}
-            />
-            <TextField
-              name="files"
-              label="Files to export"
+              name="shared"
+              label="Modules to share"
               helperText="comma seperated"
               variant="outlined"
               fullWidth
@@ -184,75 +218,54 @@ const NewApp = () => {
               inputRef={register()}
               className={classes.textField}
             />
-
-            {/*<Controller*/}
-            {/*  as={*/}
-            {/*    <FormControlLabel*/}
-            {/*      control={<Checkbox type="checkbox" />}*/}
-            {/*      label="Use automatic federation"*/}
-            {/*      key={"automaticFederation"}*/}
-            {/*    />*/}
-            {/*  }*/}
-            {/*  control={control}*/}
-            {/*  name="automaticFederation"*/}
-            {/*/>*/}
-            {!watch("automaticFederation") && (
-              <TextField
-                name="shared"
-                label="Modules to share"
-                helperText="comma seperated"
-                variant="outlined"
-                fullWidth
-                multiline
-                inputRef={register()}
-                className={classes.textField}
-              />
-            )}
-            {watch("automaticFederation") && (
-              <TextField
-                name="exclude"
-                label="Modules to exclude"
-                helperText="comma seperated"
-                variant="outlined"
-                fullWidth
-                multiline
-                inputRef={register()}
-                className={classes.textField}
-              />
-            )}
-            {watch("automaticFederation") && (
-              <TextField
-                name="singleton"
-                label="Singletons"
-                helperText="comma seperated"
-                variant="outlined"
-                fullWidth
-                multiline
-                inputRef={register()}
-                className={classes.textField}
-              />
-            )}
-          </Grid>
-          <Grid item xs={9}>
-            <Paper elevation={3} className={classes.configSection}>
-              <Typography variant="h5" className={classes.title}>
-                Webpack Configuration
-              </Typography>
-              <Typography variant="body1" className={classes.explanation}>
-                Add this code to your Webpack configuration file.
-              </Typography>
-              <CodeWrapper>
-                <GeneratedCode>{boilerplate}</GeneratedCode>
-                <Code>
-                  {`// webpack config`}
-                  <br />
-                  {`plugins: [`}
-                </Code>
-                <GeneratedCode>{codeSample}</GeneratedCode>
-                <Code>{`]`}</Code>
-              </CodeWrapper>
-            </Paper>
-          </Grid>
+          )}
+          {watch("automaticFederation") && (
+            <TextField
+              name="exclude"
+              label="Modules to exclude"
+              helperText="comma seperated"
+              variant="outlined"
+              fullWidth
+              multiline
+              inputRef={register()}
+              className={classes.textField}
+            />
+          )}
+          {watch("automaticFederation") && (
+            <TextField
+              name="singleton"
+              label="Singletons"
+              helperText="comma seperated"
+              variant="outlined"
+              fullWidth
+              multiline
+              inputRef={register()}
+              className={classes.textField}
+            />
+          )}
+        </Grid>
+        <Grid item xs={9}>
+          <Paper elevation={3} className={classes.configSection}>
+            <Typography variant="h5" className={classes.title}>
+              Webpack Configuration
+            </Typography>
+            <Typography variant="body1" className={classes.explanation}>
+              Add this code to your Webpack configuration file.
+            </Typography>
+            <CodeWrapper>
+              <GeneratedCode>{boilerplate}</GeneratedCode>
+              <Code>
+                {`// webpack config`}
+                <br />
+                {`plugins: [`}
+              </Code>
+              <GeneratedCode>{codeSample}</GeneratedCode>
+              <GeneratedCode>{dashboardPlugin}</GeneratedCode>
+              <Code>{`]`}</Code>
+            </CodeWrapper>
+          </Paper>
+        </Grid>
+        {externalApplications.length > 0 && (
           <Grid item xs={3}>
             <Typography variant="h5">Import Modules</Typography>
             <FormGroup aria-label="Imported applications">
@@ -279,23 +292,23 @@ const NewApp = () => {
               ))}
             </FormGroup>
           </Grid>
-          <Grid item xs={9}>
-            {applications.length > 0 && (
-              <Paper elevation={3} className={classes.configSection}>
-                <Typography variant="h5" className={classes.title}>
-                  Script Tags
-                </Typography>
-                <Typography variant="body1" className={classes.explanation}>
-                  Add these script tags to your page template.
-                </Typography>
-                <CodeWrapper>
-                  <GeneratedCode>{scriptTags}</GeneratedCode>
-                </CodeWrapper>
-              </Paper>
-            )}
-          </Grid>
+        )}
+        <Grid item xs={9}>
+          {applications.length > 0 && (
+            <Paper elevation={3} className={classes.configSection}>
+              <Typography variant="h5" className={classes.title}>
+                Script Tags
+              </Typography>
+              <Typography variant="body1" className={classes.explanation}>
+                Add these script tags to your page template.
+              </Typography>
+              <CodeWrapper>
+                <GeneratedCode>{scriptTags}</GeneratedCode>
+              </CodeWrapper>
+            </Paper>
+          )}
         </Grid>
-      )}
+      </Grid>
     </Layout>
   );
 };
