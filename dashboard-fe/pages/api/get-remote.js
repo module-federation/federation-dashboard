5e303684e108cfd6a9b12f66c4c6cbe57ee9750a
zackjackson <zackary.l.jackson@gmail.com>
2021-09-19 22:01:43 -0700

    set public cache

--- dashboard-fe/pages/api/get-remote.js
+++ dashboard-fe/pages/api/get-remote.js
@@ -3,15 +3,15 @@ import { privateConfig } from "../../src/config";
 export default (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
-  res.setHeader("Cache-Control", "s-maxage=60000, stale-while-revalidate=59");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=60000, stale-while-revalidate=59"
+  );
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
