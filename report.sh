3a1e3b9d769afee7b6a44c878f4ddb2df8d44a63
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-08-09 12:45:41 -0700

    debugging version setup

--- report.sh
+++ report.sh
@@ -1,6 +1,6 @@
 # The `dashboard-fe` server needs to be up and running to do this
-curl "http://localhost:3000/update" -X POST -d @dashboard-example/utils/dist/dashboard.json -H "Content-type: application/json"
-curl "http://localhost:3000/update" -X POST -d @dashboard-example/dsl/dist/dashboard.json -H "Content-type: application/json"
-curl "http://localhost:3000/update" -X POST -d @dashboard-example/home/dist/dashboard.json -H "Content-type: application/json"
-curl "http://localhost:3000/update" -X POST -d @dashboard-example/search/dist/dashboard.json -H "Content-type: application/json"
-curl "http://localhost:3000/update" -X POST -d @dashboard-example/nav/dist/dashboard.json -H "Content-type: application/json"
+curl "http://localhost:3333/update" -X POST -d @dashboard-example/utils/dist/dashboard.json -H "Content-type: application/json"
+curl "http://localhost:3333/update" -X POST -d @dashboard-example/dsl/dist/dashboard.json -H "Content-type: application/json"
+curl "http://localhost:3333/update" -X POST -d @dashboard-example/home/dist/dashboard.json -H "Content-type: application/json"
+curl "http://localhost:3333/update" -X POST -d @dashboard-example/search/dist/dashboard.json -H "Content-type: application/json"
+curl "http://localhost:3333/update" -X POST -d @dashboard-example/nav/dist/dashboard.json -H "Content-type: application/json"
