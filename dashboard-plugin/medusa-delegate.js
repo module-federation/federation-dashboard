c152d9b19a9d32f65dce6bc775584a01d1d0b636
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-12-02 13:05:13 -0800

    feat: medusa delegate module

--- dashboard-plugin/medusa-delegate.js
+++ dashboard-plugin/medusa-delegate.js
@@ -1,29 +1,27 @@
-module.exports = ({fetchClient, currentHost, remoteName})=> new Promise((resolve, reject) => {
-  fetchClient(`${process.env.dashboardUrl}&currentHost=${currentHost}&remoteName=${remoteName}`, {
+module.exports = ({fetchClient, currentHost, remoteName, dashboardURL})=> new Promise((resolve, reject) => {
+  fetchClient(`${dashboardURL}&currentHost=${currentHost}&remoteName=${remoteName}`, {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
     })
-      .then(function (res) {
-        return res.json()
-      })
-      .then(function (data) {
-        var name = data.name + "_" + data.version;
-        var filename = data.version + '.remoteEntry.js';
-        var url = new URL(filename, data.remoteURL)
+      .then((res) => res.json())
+      .then((data) => {
+        const name = `${data.name}_${data.version}`;
+        const filename = `${data.version}.remoteEntry.js`;
+        const url = new URL(filename, data.remoteURL);
 
-        new Promise(function (resolve, reject) {
-          var __webpack_error__ = new Error()
-          if (typeof window[name] !== 'undefined') return resolve();
+        new Promise((resolve, reject) => {
+          const __webpack_error__ = new Error();
+          if (typeof window[name] !== 'undefined') return resolve( window[name] );
           __webpack_require__.l(
             url.href,
-            function (event) {
-              if (typeof window[name] !== 'undefined') return resolve();
-              var errorType = event && (event.type === 'load' ? 'missing' : event.type);
-              var realSrc = event && event.target && event.target.src;
+            (event) => {
+              if (typeof window[name] !== 'undefined') return resolve( window[name] );
+              const errorType = event && (event.type === 'load' ? 'missing' : event.type);
+              const realSrc = event?.target?.src;
               __webpack_error__.message =
-                'Loading script failed.\\n(' + errorType + ': ' + realSrc + ')';
+                `Loading script failed.\\n(${errorType}: ${realSrc})`;
               __webpack_error__.name = 'ScriptExternalLoadError';
               __webpack_error__.type = errorType;
               __webpack_error__.request = realSrc;
@@ -31,7 +29,7 @@ module.exports = ({fetchClient, currentHost, remoteName})=> new Promise((resolve
             },
             name,
           );
-        }).then(function () {
+        }).then(() => {
           resolve(window[name])
         }).catch(reject)
       })
