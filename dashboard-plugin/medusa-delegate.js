72753c62fb661a26eb0d8357032f92efee73a3f9
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-03-10 15:59:41 -0800

    updating delegate module for next support

--- dashboard-plugin/medusa-delegate.js
+++ dashboard-plugin/medusa-delegate.js
@@ -1,37 +1,6 @@
-const {importDelegatedModule} = require('@module-federation/utilities');
-
-module.exports = ({fetchClient, environment, currentHost, remote,token})=>new Promise((resolve, reject) => {
+module.exports = ({fetchClient, environment, currentHost, remote,token, apiHost = "https://api.medusa.codes"})=>new Promise((resolve, reject) => {
   let f = fetchClient || fetch;
   let env = environment || process.env.NODE_ENV;
-  console.log('####medusa internals');
-  console.log({
-    'process.env.CURRENT_HOST': process.env.CURRENT_HOST,
-    'currentHost': currentHost,
-    'process.env.NODE_ENV': process.env.NODE_ENV,
-    'environment': environment,
-    'remote': remote,
-    'token': token,
-    requestURL: `https://api.medusa.codes/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost || process.env.CURRENT_HOST}`,
-    fetchClient: fetchClient,
-  });
-  console.log('#########')
-  return f(`https://api.medusa.codes/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost || process.env.CURRENT_HOST}`).then(res=>res.json()).then((remote) => {
-    console.log('medusaResponse', remote);
-  });
-  //Splitting the currentRequest using "@" as the separator and assigning the values to "global" and "url"
-  const [global, url] = currentRequest.split('@');
-  //importing the delegated module
-  importDelegatedModule({
-    global,
-    url,
-  })
-    .then(async (remote) => {
-      //resolving the remote
-      resolve(remote)
-    })
-    .catch((err) => {
-      //catching the error and rejecting it
-      reject(err);
-    });
+  f(`${apiHost}/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost || process.env.CURRENT_HOST}`).then(res=>res.json()).then(resolve).catch(reject);
 });
 
