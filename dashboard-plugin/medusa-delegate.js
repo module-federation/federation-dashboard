92eff8d10533a3869bd0c32775d4c8ccd294af4b
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-03-13 18:00:39 -0700

    feat: implement updated api into medusa delegate

--- dashboard-plugin/medusa-delegate.js
+++ dashboard-plugin/medusa-delegate.js
@@ -1,6 +1,32 @@
 module.exports = ({fetchClient, environment, currentHost, remote,token, apiHost = "https://api.medusa.codes"})=>new Promise((resolve, reject) => {
   let f = fetchClient || fetch;
   let env = environment || process.env.NODE_ENV;
-  f(`${apiHost}/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost || process.env.CURRENT_HOST}`).then(res=>res.json()).then(resolve).catch(reject);
-});
+  f(`${apiHost}/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost || process.env.CURRENT_HOST}`).then(res=>res.json())
+      .then((data) => {
+        const name = `${data.name}_${data.version}`;
+        const filename = `${data.version}.remoteEntry.js`;
+        const url = new URL(filename, data.remoteURL);
 
+        new Promise((resolve, reject) => {
+          const __webpack_error__ = new Error();
+          if (typeof window[name] !== 'undefined') return resolve( window[name] );
+          __webpack_require__.l(
+            url.href,
+            (event) => {
+              if (typeof window[name] !== 'undefined') return resolve( window[name] );
+              const errorType = event && (event.type === 'load' ? 'missing' : event.type);
+              const realSrc = event?.target?.src;
+              __webpack_error__.message =
+                `Loading script failed.\\n(${errorType}: ${realSrc})`;
+              __webpack_error__.name = 'ScriptExternalLoadError';
+              __webpack_error__.type = errorType;
+              __webpack_error__.request = realSrc;
+              reject(__webpack_error__);
+            },
+            name,
+          );
+        }).then(() => {
+          resolve(window[name])
+        }).catch(reject)
+      })
+  })
