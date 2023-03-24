51a316075005793d145c649ace0a9906e7628dcb
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-03-24 10:33:24 -0700

    fix: adding hot reload tracking to medusa runtime interfaces

--- dashboard-plugin/medusa-client.js
+++ dashboard-plugin/medusa-client.js
@@ -9,11 +9,18 @@ module.exports = ({
   new Promise((resolve, reject) => {
     let f = fetchClient || fetch;
     let env = environment || process.env.NODE_ENV;
+    const fullQuery = `${apiHost}/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost ||
+    process.env.CURRENT_HOST}`
     f(
-      `${apiHost}/env/${env}/get-remote?token=${token}&remoteName=${remote}&currentHost=${currentHost ||
-        process.env.CURRENT_HOST}`
+      fullQuery
     )
       .then(res => res.json())
-      .then(resolve)
+      .then(json=>{
+        if(global && global.__remote_scope__) {
+          global.__remote_scope__._medusa = global.__remote_scope__._medusa || {};
+          global.__remote_scope__._medusa[fullQuery] = json
+        }
+        resolve(json)
+      })
       .catch(reject);
   });
